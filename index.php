cc0e54055f61b9256d40f5e4cc29bcd4325d50dc
Sebastien Poitrenaud <poitrenaud@mac.com>
2023-03-29 15:54:20 +0200

    Conflict2

--- index.php
+++ index.php
@@ -12,10 +12,6 @@
   </head>
   <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<<<<<<< Updated upstream
     <h1>Hello, world2!</h1>
-=======
-    <script src="index.js"></script>
->>>>>>> Stashed changes
   </body>
 </html>
