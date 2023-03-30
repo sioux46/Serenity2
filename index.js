5bfcab7ec003f951567536350d836f1be0b4c872
Sebastien Poitrenaud <poitrenaud@mac.com>
2023-03-30 13:11:52 +0200

    tree format import xls

--- index.js
+++ index.js
@@ -21,4 +21,12 @@ $(document).ready(function () {
 //  ****************************************************************************
 
 var treeTest = [{a: [{b: []}, {c: []}, {d: [{e: []}]}]}];
-var tt = [['a', [['b', []], ['c', []], ['d', [['e', []]]]]]];
+
+var tt = ['a', [['b', []], ['c', []], ['d', [['e', []]]]]];
+var tt2 =
+  ['a',
+    ['b',
+      ['x',[], 'y',[]] ], ['c', []], ['d',
+        ['e', []]]];
+
+var tt3 = [["BESOINS"],["BESOINS","PERSONNE"],["BESOINS","PERSONNE","CONDUCTEUR"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","VISUELLES"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","VISUELLES","Acuité visuelle"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","VISUELLES","Daltonisme"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","VISUELLES","Sensibilité aux contrastes"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","AUDITIVES"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","ATTENTION / CONCENTRATION"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","REFLEXES / REACTIONS"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","PHYSIQUES"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","LOGIQUE"],["BESOINS","PERSONNE","CONDUCTEUR","CAPACITES","MECANIQUE"],["BESOINS","PERSONNE","CONDUCTEUR","PERSONNALITE"],["BESOINS","PERSONNE","CONDUCTEUR","PERSONNALITE","Calme"],["BESOINS","PERSONNE","CONDUCTEUR","PERSONNALITE","Tendu"],["BESOINS","PERSONNE","CONDUCTEUR","PERSONNALITE","Imprudent"],["BESOINS","PERSONNE","CONDUCTEUR","PERSONNALITE","Agité"],["BESOINS","PERSONNE","CONDUCTEUR","PERSONNALITE","Triste"]];
