5b2658f67b223045788dfa6f3b1a4612b380c951
Sebastien Poitrenaud <poitrenaud@mac.com>
2023-06-25 18:39:52 +0200

    chat key secure

--- chatgpttest.php
+++ chatgpttest.php
@@ -1,6 +1,6 @@
 <?php
 
-$apiKey = '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';
+ $apiKey = $_SERVER['OPENAI_API_KEY'];
 
 $url = 'https://api.openai.com/v1/chat/completions';
 $headers = array(
@@ -41,107 +41,4 @@ if ($response === false) {
     $assistantReply = end($responseData['choices'])['message']['content'];
     echo 'Norbert: ' . $assistantReply;
 }
-
-
-/*
-// Set your OpenAI API key
-$apiKey = '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';
-
-// Set the endpoint URL
-// $endpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-$endpoint = 'https://api.openai.com/v1/text-davinci-003/completions';
-// Set the request data
-$data = [
-  "messages" => [
-      ["role" => "system", "content" => "You are a helpful assistant."],
-      ["role" => "user", "content" => "Who won the world series in 2020?"]
-  ]
-];
-
-// Set the request headers
-$headers = [
-    'Content-Type: application/json',
-    'Authorization: Bearer ' . $apiKey,
-];
-
-// Initialize cURL
-$ch = curl_init();
-
-// Set the cURL options
-curl_setopt($ch, CURLOPT_URL, $endpoint);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// Execute the request
-$response = curl_exec($ch);
-
-// Check for cURL error
-if (curl_errno($ch)) {
-    echo 'cURL error: ' . curl_error($ch);
-} else {
-    // Decode the JSON response
-    $responseData = json_decode($response, true);
-
-    // Access the generated text
-    $generatedText = $responseData['choices'][0]['text'];
-    // Output the generated text
-    echo $generatedText;
-}
-
-// Close cURL
-curl_close($ch);
-*/
-
-
-
-
-/*
-// Set your OpenAI API key
-$apiKey = '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';
-
-// Set the endpoint URL
-$endpoint = 'https://api.openai.com/v1/engines/davinci/completions';
-
-// Set the request data
-$data = [
-    'prompt' => 'Je prends des vacances',
-    'max_tokens' => 100
-];
-
-// Set the request headers
-$headers = [
-    'Content-Type: application/json',
-    'Authorization: Bearer ' . $apiKey,
-];
-
-// Create the stream context with headers
-$context = stream_context_create([
-    'http' => [
-        'header' => $headers,
-        'method' => 'POST',
-        'content' => json_encode($data),
-    ],
-]);
-
-// Make the API request
-$response = file_get_contents($endpoint, false, $context);
-
-// Process the response
-if ($response === false) {
-    // Handle error
-    echo 'Error: Unable to fetch API response.';
-} else {
-    // Decode the JSON response
-    $responseData = json_decode($response, true);
-
-    // Access the generated text
-    $generatedText = $responseData['choices'][0]['text'];
-
-    // Output the generated text
-    echo $generatedText;
-}
-*/
-
 ?>
