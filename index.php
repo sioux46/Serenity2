02df4702e18d3944ffdd461aeaf27ce6e35a4b42
Poitrenaud Sebastien <poitrenaud@mac.com>
2024-02-19 09:53:02 +0100

    deva.scss

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <script src="evo-calendar.js"></script>
     <!--         -->
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="deva.css">
     <!-- bootdey -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" integrity="sha512-pVCM5+SN2+qwj36KonHToF2p1oIvoU3bsqxphdOIWMYmgr4ZqD3t5DjKvvetKhXGc/ZG5REYTT6ltKfExEei/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.css" integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc=" crossorigin="anonymous" />
